a4393a50f46e2af417ecbe15aa6eeec70d2a1aa5
keitakimura <s2322080@stu.musashino-u.ac.jp>
2024-01-15 21:34:29 +0900

    いらないデータを削除

---
+++ scraping2.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+# データベースへの接続
+con = sqlite3.connect('scrap_data.db')
+cur = con.cursor()
+
+# 'columnName' 列の中に 'targetString' を含む行を削除するSQL文
+sql_delete_rows = "DELETE FROM sql_DS WHERE Times LIKE '%11.0%' OR Times LIKE '%12.0%' OR Times LIKE '%13.0%' OR Times LIKE '%14.0%' OR Times LIKE '%15.0%' OR Times LIKE '%16.0%' OR Times LIKE '%17.0%' OR Times LIKE '%18.0%' OR Times LIKE '%19.0%' OR Times LIKE '%20.0%' OR Times LIKE '%21.0%' OR Times LIKE '%22.0%';"
+
+# SQL文を実行
+cur.execute(sql_delete_rows)
+
+# トランザクションのコミット
+con.commit()
+
+# データベースとの接続を閉じる
+con.close()
